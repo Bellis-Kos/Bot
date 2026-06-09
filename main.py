6d871775690b6c3fed646c806b18f1ef57e406ac
Bellis-Kos <kostmpelcrp@gmail.com>
2026-06-09 10:48:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def keep_alive():
     t.start()
 
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+token = os.getenv("DISCORD_TOKEN") or os.environ.get("DISCORD_TOKEN") 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 intents = discord.Intents.default()
